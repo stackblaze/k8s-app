157c2f377e520e6806ca20df7a595297a2d5b1e8
stackblaze <119256201+stackblaze@users.noreply.github.com>
2023-05-11 14:15:04 +0000

    .

--- index.js
+++ index.js
@@ -3,53 +3,74 @@ const { Client } = require('pg');
 
 const app = express();
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 80;
 const databaseUrl = process.env.DATABASE_URL;
 
 const client = new Client({
   connectionString: databaseUrl,
 });
 
+function generateFakeData() {
+  const fakeData = [];
+  for (let i = 0; i < 10; i++) {
+    const name = `User ${i + 1}`;
+    const email = `user${i + 1}@example.com`;
+    const password = `password${i + 1}`;
+    fakeData.push([name, email, password]);
+  }
+  return fakeData;
+}
+
 app.get('/', (req, res) => {
   client.connect()
     .then(() => console.log('Connected to PostgreSQL server'))
     .catch((err) => console.error('Error connecting to PostgreSQL server', err));
 
-  const createTableQuery = `
-    CREATE TABLE IF NOT EXISTS users (
-      id SERIAL PRIMARY KEY,
-      name VARCHAR(50) NOT NULL,
-      email VARCHAR(50) NOT NULL UNIQUE,
-      password VARCHAR(50) NOT NULL
-    )
+  const checkTableQuery = `
+    SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'users')
   `;
-  client.query(createTableQuery)
-    .then(() => console.log('Created table: users'))
-    .catch((err) => console.error('Error creating table', err));
-
-  const insertDataQuery = `
-    INSERT INTO users (name, email, password)
-    VALUES 
-      ('John Doe', 'john.doe@example.com', '123456'),
-      ('Jane Doe', 'jane.doe@example.com', 'password'),
-      ('Bob Smith', 'bob.smith@example.com', 'secret')
-  `;
-  client.query(insertDataQuery)
-    .then(() => console.log('Populated table with data'))
-    .catch((err) => console.error('Error populating table with data', err));
-
-  const selectDataQuery = `
-    SELECT * FROM users LIMIT 3
-  `;
-  client.query(selectDataQuery)
+  client.query(checkTableQuery)
     .then((result) => {
-      console.log('Read data from table');
-      res.send(result.rows);
+      const tableExists = result.rows[0].exists;
+      if (!tableExists) {
+        const createTableQuery = `
+          CREATE TABLE users (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(50) NOT NULL,
+            email VARCHAR(50) NOT NULL UNIQUE,
+            password VARCHAR(50) NOT NULL
+          )
+        `;
+        client.query(createTableQuery)
+          .then(() => console.log('Created table: users'))
+          .catch((err) => console.error('Error creating table', err));
+
+        const insertDataQuery = `
+          INSERT INTO users (name, email, password)
+          VALUES ($1, $2, $3)
+        `;
+        const fakeData = generateFakeData();
+        client.query(insertDataQuery, fakeData)
+          .then(() => console.log('Populated table with fake data'))
+          .catch((err) => console.error('Error populating table with fake data', err));
+      } else {
+        console.log('Table already exists. Skipping fake data generation.');
+      }
+
+      const selectDataQuery = `
+        SELECT * FROM users LIMIT 3
+      `;
+      client.query(selectDataQuery)
+        .then((result) => {
+          console.log('Read data from table');
+          res.send(result.rows);
+        })
+        .catch((err) => console.error('Error reading data from table', err))
+        .finally(() => {
+          client.end();
+        });
     })
-    .catch((err) => console.error('Error reading data from table', err))
-    .finally(() => {
-      client.end();
-    });
+    .catch((err) => console.error('Error checking table existence', err));
 });
 
 app.listen(port, () => {
