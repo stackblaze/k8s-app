8c5ac7d84f0bc053d0235167623520a19adae4f2
stackblaze <119256201+stackblaze@users.noreply.github.com>
2023-05-11 15:00:19 +0000

    .

--- index.js
+++ index.js
@@ -6,6 +6,55 @@ const app = express();
 const port = process.env.PORT || 80;
 const databaseUrl = process.env.DATABASE_URL;
 
+const client = new Client({
+  connectionString: databaseUrl,
+});
+
+async function connectAndOperate() {
+  try {
+    await client.connect();
+    console.log('Connected to PostgreSQL server');
+
+    const checkTableQuery = `
+      SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'users')
+    `;
+    const tableCheckResult = await client.query(checkTableQuery);
+    const tableExists = tableCheckResult.rows[0].exists;
+
+    if (!tableExists) {
+      const createTableQuery = `
+        CREATE TABLE users (
+          id SERIAL PRIMARY KEY,
+          name VARCHAR(50) NOT NULL,
+          email VARCHAR(50) NOT NULL UNIQUE,
+          password VARCHAR(50) NOT NULL
+        )
+      `;
+      await client.query(createTableQuery);
+      console.log('Created table: users');
+
+      const insertDataQuery = `
+        INSERT INTO users (name, email, password)
+        VALUES ($1, $2, $3)
+      `;
+      const fakeData = generateFakeData();
+      await client.query(insertDataQuery, fakeData);
+      console.log('Populated table with fake data');
+    } else {
+      console.log('Table already exists. Skipping fake data generation.');
+    }
+
+    const selectDataQuery = `
+      SELECT * FROM users LIMIT 3
+    `;
+    const selectResult = await client.query(selectDataQuery);
+    console.log('Read data from table');
+    return selectResult.rows;
+  } catch (err) {
+    console.error('Error performing database operations', err);
+  }
+}
+
 function generateFakeData() {
   const fakeData = [];
   for (let i = 0; i < 10; i++) {
@@ -17,62 +66,18 @@ function generateFakeData() {
   return fakeData;
 }
 
-app.get('/', (req, res) => {
-  const client = new Client({
-    connectionString: databaseUrl,
+app.get('/', async (req, res) => {
+  const data = await connectAndOperate();
+  res.send(data);
+});
+
+client.connect()
+  .then(() => {
+    console.log('PostgreSQL client connected');
+    app.listen(port, () => {
+      console.log(`Server listening on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Error connecting to PostgreSQL server', err);
   });
-
-  client.connect()
-    .then(() => console.log('Connected to PostgreSQL server'))
-    .catch((err) => console.error('Error connecting to PostgreSQL server', err));
-
-  const checkTableQuery = `
-    SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'users')
-  `;
-  client.query(checkTableQuery)
-    .then((result) => {
-      const tableExists = result.rows[0].exists;
-      if (!tableExists) {
-        const createTableQuery = `
-          CREATE TABLE users (
-            id SERIAL PRIMARY KEY,
-            name VARCHAR(50) NOT NULL,
-            email VARCHAR(50) NOT NULL UNIQUE,
-            password VARCHAR(50) NOT NULL
-          )
-        `;
-        client.query(createTableQuery)
-          .then(() => console.log('Created table: users'))
-          .catch((err) => console.error('Error creating table', err));
-
-        const insertDataQuery = `
-          INSERT INTO users (name, email, password)
-          VALUES ($1, $2, $3)
-        `;
-        const fakeData = generateFakeData();
-        client.query(insertDataQuery, fakeData)
-          .then(() => console.log('Populated table with fake data'))
-          .catch((err) => console.error('Error populating table with fake data', err));
-      } else {
-        console.log('Table already exists. Skipping fake data generation.');
-      }
-
-      const selectDataQuery = `
-        SELECT * FROM users LIMIT 3
-      `;
-      client.query(selectDataQuery)
-        .then((result) => {
-          console.log('Read data from table');
-          res.send(result.rows);
-        })
-        .catch((err) => console.error('Error reading data from table', err))
-        .finally(() => {
-          client.end();
-        });
-    })
-    .catch((err) => console.error('Error checking table existence', err));
-});
-
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
